ddb51c48ce5f8ff7b7e7df690e105caf5745038e
Ymin136 <hart1364@naver.com>
2022-09-13 11:57:38 +0900

    jym0913

--- src/main/java/com/gogangdo/MainController.java
+++ src/main/java/com/gogangdo/MainController.java
@@ -12,7 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.gogangdo.dto.FileDTO;
 import com.gogangdo.dto.MemberDTO;
 import com.gogangdo.dto.ProductDTO;
 import com.gogangdo.service.MemberService;
@@ -116,11 +119,20 @@ public class MainController {
 		fis.close();
 		bos.close();
 	}
+		@RequestMapping("/productDetail.do")
+		public String productDetail(int product_no, Model model) {
+			ProductDTO dto = productService.selectproductDTO(product_no);
+			FileDTO thumbnail = productService.selectThumbnailDTO(product_no);
+			FileDTO image = productService.selectimageDTO(product_no);
+			System.out.println(dto.toString());
+			System.out.println(thumbnail.toString());
+			System.out.println(image.toString());
+			model.addAttribute("product", dto);
+			model.addAttribute("thumbnail", thumbnail);
+			model.addAttribute("image", image);
+			return "product_detail";
+		}
 	
-	@RequestMapping("/productDetail.do")
-	public String productDetail() {
-		return "product_detail";
-	}
 	@RequestMapping("/myPage.do")
 	public String myPage() {
 		return "mypage";
@@ -140,16 +152,41 @@ public class MainController {
 	}
 	
 	@RequestMapping("/productRegister.do")
-	public void productRegister(ProductDTO dto, HttpServletResponse response) {		
+	public void productRegister(ProductDTO dto, HttpServletResponse response, MultipartHttpServletRequest request) throws IOException {
+		try {
 		int pno = productService.selectProductNo();
 		dto.setProduct_no(pno);
 		System.out.println(dto.toString());
+		dto.setProduct_desc(dto.getProduct_desc().replaceAll("\\n", "<br>"));
 		productService.registerProduct(dto);
 		
+		
+		String root ="c:\\fileUpload\\";
+		File userRoot = new File(root);
+		if(userRoot.exists())
+			userRoot.mkdirs();
+		
+		MultipartFile thumbnail = request.getFile("product_thumbnail");
+		MultipartFile product_img = request.getFile("product_img");
+		
+		String originalFileName1 = thumbnail.getOriginalFilename();
+		String originalFileName2 = product_img.getOriginalFilename();
+		File uploadFile1 = new File(root + "\\"+originalFileName1);
+		File uploadFile2 = new File(root + "\\"+originalFileName2);
+		int img_no = productService.selectImageNo();
+		productService.insertthumbnail(new FileDTO(uploadFile1, pno, img_no));
+		img_no = productService.selectImageNo();
+		productService.insertproduct_img(new FileDTO(uploadFile2, pno, img_no));		
 		try {
-			response.getWriter().write("1");
-		} catch (IOException e) {			
+			thumbnail.transferTo(uploadFile1);
+			product_img.transferTo(uploadFile2);
+		} catch (IllegalStateException | IOException e) {
 			e.printStackTrace();
+		}
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().write("<script>alert('상품 등록 성공');location.href='productList.do';</script>");
+		} catch (IOException e) {			
+			response.getWriter().write("<script>alert('데이터입력이 잘못됐습니다.');history.bakc();</script>");
 		}
 	}
 	
